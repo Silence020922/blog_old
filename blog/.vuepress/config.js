e48e25decf2227e3fc2d47c2a252506a65d99e13
root <root@localhost.localdomain>
2022-10-09 18:01:26 +0800

    deploy

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -5,12 +5,16 @@ module.exports = {
   markdown: {
     lineNumbers: true,
     extractHeaders: [ 'h2', 'h3', 'h4' ],
-    plugins: {
-      'markdown-it-mark': true,
-      'markdown-it-footnote': true,
-      'markdown-it-abbr': true,
-      'markdown-it-task-lists': true
-    }
+    plugins: [
+      [
+        'vuepress-plugin-mathjax',
+        {
+          macros: {
+            '\\Z': '\\mathbb{Z}',
+          },
+        },
+      ],
+    ],
   },
 
   theme: require.resolve('../../index'), // 使用本地主题
